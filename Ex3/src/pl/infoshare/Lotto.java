ffe33a6517550f34b878dd68830d8f8e40e30eda
Mateusz Lesisz <m.lesisz97@wp.pl>
2021-09-27 11:48:05 +0200

    Finished ex 3

--- Ex3/src/pl/infoshare/Lotto.java
+++ Ex3/src/pl/infoshare/Lotto.java
@@ -1,9 +1,9 @@
 package pl.infoshare;
 
+import java.lang.reflect.Array;
 import java.util.*;
 
 public class Lotto {
-
 
     public static void randomNumber() throws InterruptedException {
         ArrayList numbers  = new ArrayList();
@@ -12,9 +12,12 @@ public class Lotto {
         }
         Collections.shuffle(numbers);
         for(int j = 0; j < NumbersFromUser.getSecondNumberOfRange(); j++) {
-            System.out.println(numbers.get((j)));
+            System.out.println(numbers.get(j));
             Thread.sleep(1000);
         }
+
+
+
     }
 
 }
