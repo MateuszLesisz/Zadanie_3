41d65665e66039d139075cf0b75855f8aaa76c14
Mateusz Lesisz <m.lesisz97@wp.pl>
2021-09-27 11:19:32 +0200

    Add Timer

--- Ex3/src/pl/infoshare/Lotto.java
+++ Ex3/src/pl/infoshare/Lotto.java
@@ -5,15 +5,15 @@ import java.util.*;
 public class Lotto {
 
 
-    public static void randomNumber() {
+    public static void randomNumber() throws InterruptedException {
         ArrayList numbers  = new ArrayList();
         for (int i = 0; i < NumbersFromUser.getFirstNumberOfRange(); i++) {
             numbers.add(i+1);
         }
         Collections.shuffle(numbers);
-
         for(int j = 0; j < NumbersFromUser.getSecondNumberOfRange(); j++) {
             System.out.println(numbers.get((j)));
+            Thread.sleep(1000);
         }
     }
 
--- Ex3/src/pl/infoshare/Main.java
+++ Ex3/src/pl/infoshare/Main.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         NumbersFromUser.firstNumber();
         NumbersFromUser.secondNumber();
         NumbersFromUser.writeMethod();
